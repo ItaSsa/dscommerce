47a6bb26f5a53d4c209c75779ab847e1292cbd8c
Itaira <itairassa@gmail.com>
2023-09-04 13:13:43 +0200

    Seeding database after relationships mapped

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -1,7 +1,11 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +13,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,10 +26,27 @@ public class Order {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
 	private OrderStatus status;
-	
+	 
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private User client;
+	
+	@OneToOne(mappedBy="order",cascade = CascadeType.ALL)
+	private Payment payment;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Product> getProducts() {
+		return items
+				.stream()
+				.map(x -> x.getProduct())
+				.toList();
+	}
 	
 	public Order() {
 
--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class User {
 		@GeneratedValue(strategy=GenerationType.IDENTITY)
 		private Long id;
 		private String name;
+		@Column(unique = true)
 		private String email;
 		private String phone;
 		private LocalDate birthDate;
@@ -91,8 +93,13 @@ public class User {
 			this.password = password;
 		}
 
-		public List<Order> getOrder() {
+		public List<Order> getOrders() {
 			return orders;
 		}
+
+		public void setOrders(List<Order> orders) {
+			this.orders = orders;
+		}
+
 
 }
