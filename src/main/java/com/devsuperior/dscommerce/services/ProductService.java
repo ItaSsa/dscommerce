069949895016eb94b4418747f414ae11bbe4dbe4
Itaira <itairassa@gmail.com>
2023-09-28 12:29:26 +0200

    ProductDTO with categories

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscommerce.dtos.CategoryDTO;
 import com.devsuperior.dscommerce.dtos.ProductDto;
 import com.devsuperior.dscommerce.dtos.ProductMinDTO;
+import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
@@ -82,6 +84,13 @@ public class ProductService {
 			entity.setDescription(dto.getDescription()) ;
 			entity.setPrice(dto.getPrice());
 			entity.setImgUrl(dto.getImgUrl()) ;
+			
+			entity.getCategories().clear();
+			for(CategoryDTO catDto: dto.getCategories()) {
+				Category cat = new Category();
+				cat.setId(catDto.getId());
+				entity.getCategories().add(cat);
+			}
 
 	}
 
